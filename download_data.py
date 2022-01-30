6865c3f723fa049a6a1777daa0a219484050745b
enzo-inc <vincenzo.incutti@gmail.com>
2022-01-30 13:49:42 +0000

    download 2022 papers

--- download_data.py
+++ download_data.py
@@ -1,69 +1,51 @@
 import requests
 from bs4 import BeautifulSoup
+from tika import parser
+from io import BytesIO
 import json
-from pdf2image import convert_from_bytes
-import pytesseract
 
 
-archives = ["astro-ph", "cond-mat", "cs", "econ", "eess", "gr-qc", "hep-ex", "hep-lat",
-            "hep-ph", "hep-th", "math", "math-ph", "nlin", "nucl-ex", "nucl-th",
-            "physics", "q-bio", "q-fin", "quant-ph", "stat"]
-
-url = "https://arxiv.org/list?archive=astro-ph&year=22&month=all"
-
-def extract_metadata(homepage):
+def extract_metadata(homepage, date, n_str):
     record = {}
     record["Title"] = homepage.find(class_="title").text.replace('Title:', '')
     record["Authors"] = homepage.find(class_="authors").text.replace('Authors:', '')
     record["Abstract"] = homepage.find(class_="abstract").text.replace('\nAbstract:', '').replace('\n', ' ')
     record["Subject"] = homepage.find(class_="primary-subject").text
-    record["DOI"] = f"arXiv:{id}"
+    record["ID"] = f"arXiv:{date}.{n_str}"
     return record
 
 def pdf2text(pdf):
-    text = ""
-    # Convert PDF to image
-    pages = convert_from_bytes(pdf)
-    # OCR to convert image to text
-    for page in pages:
-        text += str((pytesseract.image_to_string(page))).replace('\n', '')
-    return text
+    raw = parser.from_buffer(BytesIO(pdf.content))
+    content = raw['content']
+    return content
 
 def save_record(record, file):
     json.dump(record, file)
     file.write('\n')
 
-# GET page contatining all articles
-# This defaults to showing the first 25 results
-response = requests.get(url)
-# Page displaying all articles from 2022
-url = ""
-doc = BeautifulSoup(response.text,'html.parser')
-small_elements = doc.findAll("small")
-for e in small_elements:
-    for anchor in e.findAll("a"):
-        if anchor.text == "all":
-            url = f"https://arxiv.org{anchor.get('href')}"
+# arXiv ids follow the structure: "YYMM.xxxxx"
+years = ["22", "21", "20"]
+months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+
+# Retrieve all papers from 2022
+for month in months:
+    date = f"22{month}"
+    n = 1
+    while True:
+        n_str = str(n).zfill(5)
+        homepage_url = f"https://arxiv.org/abs/{date}.{n_str}"
+        pdf_url = f"https://arxiv.org/pdf/{date}.{n_str}.pdf"
+        print(f"Processing paper {date}.{n_str}")
+        try:
+            homepage = BeautifulSoup(requests.get(homepage_url).text, 'html.parser')
+            pdf = requests.get(pdf_url)
+            record = extract_metadata(homepage, date, n_str)
+            record['Text'] = pdf2text(pdf)
+            with open('data.ndjson', 'a') as f:
+                save_record(record, f)
+            n += 1
+        except:
+            print("Paper id limit reached")
             break
-response = requests.get(url)
-
-# Store ids of papers to download
-paper_ids = []
-doc = BeautifulSoup(response.text,'html.parser')
-papers = doc.findAll("dt")
-for paper in papers:
-    for anchors in paper.findAll("a"):
-        href = anchors.get('href')
-        if isinstance(href, str) and href.startswith('/abs'):
-            paper_ids.append(f"{href.strip('/abs/')}")
-
-# The homepage of the paper is at "arxiv/abs/<paper_id>". From here we extract metadata.
-# The PDF of the paper is at "arxiv/pdf/<paper_id>.pdf".
-with open('data.ndjson', 'w') as f:
-    for id in paper_ids:
-        response = requests.get(f"https://arxiv.org/abs/{id}")
-        homepage = BeautifulSoup(response.text, 'html.parser')
-        record = extract_metadata(homepage)
-        pdf = requests.get(f"https://arxiv.org/pdf/{id}.pdf")
-        record['Text'] = pdf2text(pdf.content)
-        save_record(record, f)
+    break
+        
