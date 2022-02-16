832371a05ecd508157c90f96cc5243b2f03f522e
Dale McDiarmid <dale@clickhouse.com>
2022-02-16 21:02:49 +0000

    Stream clean

--- cleaner.py
+++ cleaner.py
@@ -2,9 +2,7 @@ import string
 import ndjson
 import json
 import sys
-import jsonlines
 import re
-
 
 # run using python3 clean.py filename.ndjson
 
@@ -12,41 +10,30 @@ import re
 # removes newlines, words of length 1 and 2, puntuation
 
 def clean(arg):
-
-
-    with open(arg) as f:
-        data = ndjson.load(f)
-
-    clean_paper = []
-    for i in range(len(data)):   
-
-        split =  data[i].split()
-        add_to_clean = ""
-        for token in split:  
-    
-            string_encode = token.encode("ascii", "ignore").decode()           # this removes the unicodes like \uxxxx
-            token = string_encode
-            if("http" in token or "@" in token):                               #keep the url and emails
-                token = token.replace("mailto:","")
-                add_to_clean = add_to_clean + " " + token
-                
-                
-            else:
-                token = token.translate(str.maketrans('', '', "!|\"£$%^&*()_+=}{[]~:;><.,/\'"))               #this removes the punctuations
-                token = token.replace(" " ,"")
-                token = re.sub(r'[0-9]+', '', token)
-                if (len(token) <= 2 and ("figure." not in token)):
-                    do_nothing = True # do nothing i.e dont add this word 
-                else:
+    with open(arg, "r") as f, open(arg.replace(".ndjson", "") + "_clean.ndjson", "w") as clean_file:
+        for line in f:
+            data = json.loads(line)
+            split = data["body"].split()
+            add_to_clean = ""
+            for token in split:
+                string_encode = token.encode("ascii", "ignore").decode()  # this removes the unicodes like \uxxxx
+                token = string_encode
+                if "http" in token or "@" in token:  # keep the url and emails
+                    token = token.replace("mailto:", "")
                     add_to_clean = add_to_clean + " " + token
-
-        clean_paper.append(re.sub("- | -", "",add_to_clean))
-
-    arg = arg.replace(".ndjson","")
-    with open(arg+ "_clean.ndjson","w") as file:
-        ndjson.dump(clean_paper,file)                       #create the new ndjson file and write to it 
+                else:
+                    token = token.translate(
+                        str.maketrans('', '', "!|\"£$%^&*()_+=}{[]~:;><.,/\'"))  # this removes the punctuations
+                    token = token.replace(" ", "")
+                    token = re.sub(r'[0-9]+', '', token)
+                    if len(token) <= 2 and ("figure." not in token):
+                        # do nothing i.e dont add this word
+                        pass
+                    else:
+                        add_to_clean = add_to_clean + " " + token
+            data["body"] = re.sub("- | -", "", add_to_clean)
+            clean_file.write(json.dumps(data) + "\n")
 
 
 if __name__ == "__main__":
     clean(sys.argv[-1])
-    
