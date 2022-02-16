494abf2079ced58a514a367548117e1f61e01091
Dale McDiarmid <dale@clickhouse.com>
2022-02-16 21:06:05 +0000

    Use usjon

--- cleaner.py
+++ cleaner.py
@@ -1,6 +1,4 @@
-import string
-import ndjson
-import json
+import ujson as json
 import sys
 import re
 
@@ -13,25 +11,26 @@ def clean(arg):
     with open(arg, "r") as f, open(arg.replace(".ndjson", "") + "_clean.ndjson", "w") as clean_file:
         for line in f:
             data = json.loads(line)
-            split = data["body"].split()
-            add_to_clean = ""
-            for token in split:
-                string_encode = token.encode("ascii", "ignore").decode()  # this removes the unicodes like \uxxxx
-                token = string_encode
-                if "http" in token or "@" in token:  # keep the url and emails
-                    token = token.replace("mailto:", "")
-                    add_to_clean = add_to_clean + " " + token
-                else:
-                    token = token.translate(
-                        str.maketrans('', '', "!|\"£$%^&*()_+=}{[]~:;><.,/\'"))  # this removes the punctuations
-                    token = token.replace(" ", "")
-                    token = re.sub(r'[0-9]+', '', token)
-                    if len(token) <= 2 and ("figure." not in token):
-                        # do nothing i.e dont add this word
-                        pass
-                    else:
+            if "body" in data:
+                split = data["body"].split()
+                add_to_clean = ""
+                for token in split:
+                    string_encode = token.encode("ascii", "ignore").decode()  # this removes the unicodes like \uxxxx
+                    token = string_encode
+                    if "http" in token or "@" in token:  # keep the url and emails
+                        token = token.replace("mailto:", "")
                         add_to_clean = add_to_clean + " " + token
-            data["body"] = re.sub("- | -", "", add_to_clean)
+                    else:
+                        token = token.translate(
+                            str.maketrans('', '', "!|\"£$%^&*()_+=}{[]~:;><.,/\'"))  # this removes the punctuations
+                        token = token.replace(" ", "")
+                        token = re.sub(r'[0-9]+', '', token)
+                        if len(token) <= 2 and ("figure." not in token):
+                            # do nothing i.e dont add this word
+                            pass
+                        else:
+                            add_to_clean = add_to_clean + " " + token
+                data["body"] = re.sub("- | -", "", add_to_clean)
             clean_file.write(json.dumps(data) + "\n")
 
 
